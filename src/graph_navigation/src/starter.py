276291598e21a86ff64d95d05a0bacf3c56b7690
Norihiro02 <s1876011BF@s.chibakoudai.jp>
2018-10-12 00:37:08 +0900

    つくば用グラフを用意

--- src/graph_navigation/src/starter.py
+++ src/graph_navigation/src/starter.py
@@ -9,7 +9,7 @@ c=actionlib.SimpleActionClient('graph_navi',graph_naviAction);
 c.wait_for_server()
 goal=graph_naviGoal()
 goal.start=1
-goal.goal=4
+goal.goal=12
 goal.checkPoint=[6]
 c.send_goal(goal)
 c.wait_for_result()
