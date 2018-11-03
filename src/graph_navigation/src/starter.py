7b5d30a2739031a6dfe64f7c7760b7ce43b0d51a
Norihiro02 <s1876011BF@s.chibakoudai.jp>
2018-11-03 22:24:14 +0900

    第6回試走会仕様

--- src/graph_navigation/src/starter.py
+++ src/graph_navigation/src/starter.py
@@ -8,8 +8,8 @@ rospy.init_node("graph_navi_starter");
 c=actionlib.SimpleActionClient('graph_navi',graph_naviAction);
 c.wait_for_server()
 goal=graph_naviGoal()
-goal.start=0
-goal.goal=35
-goal.checkPoint=[5,36,23,20,37,30,13]
+goal.start=1
+goal.goal=10
+goal.checkPoint=[5]
 c.send_goal(goal)
 c.wait_for_result()
