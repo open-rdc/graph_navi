f76e3eee3df3c1228eee70e486984a7f8e7b3fa7
Norihiro02 <s1876011BF@s.chibakoudai.jp>
2018-11-04 12:45:48 +0900

    固定コース巡回

--- src/graph_navigation/src/starter.py
+++ src/graph_navigation/src/starter.py
@@ -8,8 +8,8 @@ rospy.init_node("graph_navi_starter");
 c=actionlib.SimpleActionClient('graph_navi',graph_naviAction);
 c.wait_for_server()
 goal=graph_naviGoal()
-goal.start=1
-goal.goal=10
-goal.checkPoint=[5]
+goal.start=0
+goal.goal=35
+goal.checkPoint=[6,23,36,37,30,9]
 c.send_goal(goal)
 c.wait_for_result()
