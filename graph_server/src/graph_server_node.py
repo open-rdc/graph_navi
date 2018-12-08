7e0947ab270e23894e1d4f2df4f8dc15ab2b6070
Norihiro02 <s1876011BF@s.chibakoudai.jp>
2018-12-08 22:31:34 +0900

    graph_serverにedge削除サービスを追加(動作未確認)

--- graph_server/src/graph_server_node.py
+++ graph_server/src/graph_server_node.py
@@ -9,50 +9,62 @@ from geometry_msgs.msg import PoseStamped
 import tf
 class graph:
 
-    def __init__(self,file_name):
+	def __init__(self,file_name):
+		
+		gFile=Graph_file();
+		self.G=gFile.read_file(file_name);
+		rospy.init_node("graph_server")
+		self.srv_minimal_path=rospy.Service("minimal_path",GraphPath,self.minimal_path);
+		self.srv_get_node_info=rospy.Service("get_node_info",NodeInfo,self.get_node_data);
+		self.srv_remove_edge=rospy.Service("remove_edge",EdgeOper,self.remove_edge_srv);
+		rospy.spin();
+		
+		return
 
-        gFile=Graph_file();
-        self.G=gFile.read_file(file_name);
-        rospy.init_node("graph_server")
-        self.srv_minimal_path=rospy.Service("minimal_path",GraphPath,self.minimal_path);
-        self.srv_get_node_info=rospy.Service("get_node_info",NodeInfo,self.get_node_data);
-        rospy.spin();
-        return
+	def minimal_path(self,node):
+		#print node.start
+		#print node.goal
+		route=nx.dijkstra_path(self.G,node.start,node.goal)
+		lange=nx.dijkstra_path_length(self.G,node.start,node.goal)
+		#print route
+		return {'path':route,'lange':lange};
 
-    def minimal_path(self,node):
-        #print node.start
-        #print node.goal
-        route=nx.dijkstra_path(self.G,node.start,node.goal)
-        lange=nx.dijkstra_path_length(self.G,node.start,node.goal)
-        #print route
-        return {'path':route,'lange':lange }
-    def point_to_pose(self,x,y,z):
-        pose=PoseStamped()
-        
-        pose.pose.position.x=x
-        pose.pose.position.y=y
-        pose.pose.position.z=z
-        
-        q=tf.transformations.quaternion_from_euler(0,0,0)
+	def add_edge(self,Edge_srv):
+		cost=get_cost(Edge_srv.edge[0],Edge_srv.edge[1]);
+		self.G.add_edge(Edge_srv.edge[0],Edge_srv.edge[1],weight=cost);
 
-        pose.pose.orientation.x=q[0];
-        pose.pose.orientation.y=q[1];
-        pose.pose.orientation.z=q[2];
-        pose.pose.orientation.w=q[3];
+	def get_cost(self,nodeA,nodeB):
+		return 0;
+	def remove_edge_srv(self,Edge_srv):
+		self.G.remove_edge(Edge_srv.edge[0],Edge_srv.edge[1]);
+		return {'status':0}
+		
+	def point_to_pose(self,x,y,z):
+		pose=PoseStamped()
+		pose.pose.position.x=x
+		pose.pose.position.y=y
+		pose.pose.position.z=z
+		q=tf.transformations.quaternion_from_euler(0,0,0)
+		
+		pose.pose.orientation.x=q[0];
+		pose.pose.orientation.y=q[1];
+		pose.pose.orientation.z=q[2];
+		pose.pose.orientation.w=q[3];
+		return pose
+		
+	def get_node_data(self,node_name):
+		
+		node=self.G.nodes[node_name.node_id]
+		
+		if 'Point'in node:
+			pose=self.point_to_pose(node['Point']['point']['x'],node['Point']['point']['y'],node['Point']['point']['z'])
+		else:
+			pose=self.point_to_pose(node['Pose']['pose']['position']['x'],node['Pose']['pose']['position']['y'],node['Pose']['pose']['position']['z'])
+			
+		return {'pose':pose}
 
-        return pose
-    def get_node_data(self,node_name):
-        node=self.G.nodes[node_name.node_id]
-
-        if 'Point'in node:
-            pose=self.point_to_pose(node['Point']['point']['x'],node['Point']['point']['y'],node['Point']['point']['z'])
-        else:
-            pose=self.point_to_pose(node['Pose']['pose']['position']['x'],node['Pose']['pose']['position']['y'],node['Pose']['pose']['position']['z'])
-        
-        return {'pose':pose}
 if __name__=="__main__":
-
-    rospy.init_node("graph_server")
-    graph_file_path=rosparam.get_param("graph_file");
-    grp=graph(graph_file_path);
+	rospy.init_node("graph_server")
+	graph_file_path=rosparam.get_param("graph_file");
+	grp=graph(graph_file_path);
 
